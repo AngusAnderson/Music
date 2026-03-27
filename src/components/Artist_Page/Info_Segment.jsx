780f068609528d2a82dfb8ec0838b65bde4d88b2
Angus the Anderson <95943624+AngusAnderson@users.noreply.github.com>
2026-03-27 19:48:11 +0000

    Fix CSS import case sensitivity in Info_Segment

--- src/components/Artist_Page/Info_Segment.jsx
+++ src/components/Artist_Page/Info_Segment.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Genre_carousel from '../Genre_Carousel';
 import Location_And_Year_Card from './Location_And_Year_Card';
 import Credit_Card from './Credit_Card';
-import '../../css/Artist_Page/info_segment.css'
+import '../../css/Artist_Page/Info_Segment.css'
 
 
 const Info_Segment = () => {

135926628cce19c8e5a06a0f7c966e8fafd8422a
Angus the Anderson <95943624+AngusAnderson@users.noreply.github.com>
2026-03-27 19:46:49 +0000

    Fix import case for Genre_Carousel component

--- src/components/Artist_Page/Info_Segment.jsx
+++ src/components/Artist_Page/Info_Segment.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Genre_carousel from '../Genre_carousel';
+import Genre_carousel from '../Genre_Carousel';
 import Location_And_Year_Card from './Location_And_Year_Card';
 import Credit_Card from './Credit_Card';
 import '../../css/Artist_Page/info_segment.css'

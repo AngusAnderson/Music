c5e57b571ac908c93047033821b3cec684d07c91
Angus Anderson <angus.anderson.2006@gmail.com>
2026-03-28 01:23:55 +0000

    feat: Modified console and fixed class name issue

--- src/components/buttons/Play_Buttons.jsx
+++ src/components/buttons/Play_Buttons.jsx
@@ -16,7 +16,7 @@ const Play_Button = () => {
       <div className="button-container">
         <audio ref={audioRef} src={clickSound} />
         <button className='btn-3d' onClick={handleClick}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className='play-svg' class="bi bi-play-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-play-fill play-svg" viewBox="0 0 16 16">
                 <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>
             </svg>
         </button>

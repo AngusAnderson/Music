634fd583a2425e3bee0c1438f15466bb09f96378
Angus Anderson <angus.anderson.2006@gmail.com>
2026-03-28 00:51:27 +0000

    feat: Added play button and sound effect when clicked

--- src/components/Name_Card.jsx
+++ src/components/Name_Card.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Play_Button from './buttons/Play_Buttons'
 
 const Name_Card = ({ name }) => {
   return (
     <div className='info'>
       <h1>{name}</h1>
+      <Play_Button />
     </div>
   )
 }
---
+++ src/components/buttons/Play_Buttons.jsx
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react'
+import '../../css/buttons/Play_Button.css'
+import clickSound from '../../assets/sounds/clickSound.wav'
+
+const Play_Button = () => {
+    const audioRef = useRef(null)
+  
+    const handleClick = () => {
+      if (audioRef.current) {
+        audioRef.current.currentTime = 0
+        audioRef.current.play()
+      }
+    }
+  
+    return (
+      <div className="button-container">
+        <audio ref={audioRef} src={clickSound} />
+        <button className='btn-3d' onClick={handleClick}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className='play-svg' class="bi bi-play-fill" viewBox="0 0 16 16">
+                <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>
+            </svg>
+        </button>
+      </div>
+    )
+  }
+  
+  export default Play_Button
